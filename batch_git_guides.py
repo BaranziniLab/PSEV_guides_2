36b58e0a063f21cfab5caae0d0afa88c607fb3bf
Charlotte Nelson <canelson@dipper>
2019-05-04 12:28:19 -0700

    test

---
+++ batch_git_guides.py
@@ -0,0 +1,10 @@
+import numpy as np
+import subprocess
+
+for i in range(0,10):
+	files = 'shortest_path_DB*%s.html'% i
+	subprocess.call(['git', 'commit', files, '-m', '"%s"'%i])
+	subprocess.call(['git', 'push'])
+
+
+
